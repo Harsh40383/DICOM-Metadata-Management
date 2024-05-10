4c23c935f46c5deda9b0fac87cc0686aa951747d
Harsh Jaiswal <65247237+Harsh40383@users.noreply.github.com>
2024-05-10 14:35:18 +0530

    Update forms.py

--- dicom/forms.py
+++ dicom/forms.py
@@ -12,10 +12,6 @@ class LoginForm(forms.Form):
     username = forms.CharField(label="Username", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
     password = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
 
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-
 
 
 class RegistrationForm(forms.Form):
